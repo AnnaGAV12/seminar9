a6b34634f7c3ba2ea97a889f4f15da9cf2d1d674
Anna <ds_anna@bk.ru>
2022-06-20 15:18:30 +0300

    Показать натуральные числа от N до 1, N задано

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Показать натуральные числа от N до 1, N задано
+void PrintNumber(int n)
+{
+    Console.Write(n + " ");
+    n--;
+    if(n == 0) return;
+    PrintNumber(n);
+}
+int N = 20;
+PrintNumber(N);
+
+// void Num(int n)
+//{
+//    if (n>0)
+//    {
+//     Console.WriteLine("{0}",n);
+//     Num(n-1);
+//    }
+//}
+//Console.WriteLine("Введите число ");
+//int n=int.Parse(Console.ReadLine());
+//Num(n);
